41369f5c8495aeed5a3d3465b56e5331842df623
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-23 17:05:16 +1000

    config react routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./Home";
+import Group from "./Group";
+import Dashboard from "./Dashboard";
+import Member from "./Member";
+
+
 function App() {
   return (
     <div className="App">
-      split
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/groups' element={ <Group /> } />
+          <Route path='/groups/:id' element={ <Dashboard /> } />
+          <Route path='/groups/:groups_id/members' element={ <Member /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
