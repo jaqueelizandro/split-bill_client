8c02779c698b1863fb765f6f7dcaf60a1d814022
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-25 19:02:59 +1000

    create member create component

---
+++ src/components/MemberCreate.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
+
+const MemberCreate = (props) => {
+    const [member, setMember] = useState({
+        name: '',
+        email: ''
+    });
+    const navigate = useNavigate();
+
+    const { groups_id  } = useParams();
+    const _handleSubmit = (event) => {
+        event.preventDefault();
+        fetch(`http://localhost:3000/groups/${ groups_id }/members`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(member),
+        })
+        .then((resp) => resp.json())
+        .then((result) => navigate(-1))
+    };
+
+    const _handleChange = (event) => {
+        setMember((prevMember) => ({
+            ...prevMember, [event.target.name]: event.target.value
+        })
+    )};
+    
+    return(
+        <div>
+            <form onSubmit={_handleSubmit}>
+                <p>New member</p>
+                <input name="name" value={member.name} required
+                    onChange={_handleChange} placeholder="Name" />
+                <input name="email" value={member.email} type="email" 
+                    onChange={_handleChange} placeholder="Email (optional)" />
+
+                <button>Save</button>
+            </form>
+        </div>
+    );
+};
+
+export default MemberCreate;
