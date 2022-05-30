ed5cde473f397ada2c8a72f55c5f641359145cc9
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-30 14:42:13 +1000

    create routes for edit transaction and members

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./Home";
 import Group from "./Group";
 import Dashboard from "./Dashboard";
-import MemberInicialCreate from "./MemberInicialCreate";
-import MemberDisplay from './MemberDisplay';
+import Debts from './Debts';
+import Settle from './Settle';
 import TransactionDisplay from './TransactionDisplay';
+import TransactionCreate from './TransactionCreate';
+import TransactionEdit from './TransactionEdit';
+import MemberDisplay from './MemberDisplay';
+import MemberCreate from './MemberCreate';
+import MemberEdit from './MemberEdit';
+import MemberInicialCreate from "./MemberInicialCreate";
 
 
 function App() {
@@ -14,10 +20,16 @@ function App() {
         <Routes>
           <Route path='/' element={ <Home /> } />
           <Route path='/groups' element={ <Group /> } />
-          <Route path='/groups/:id' element={ <Dashboard /> } />
-          <Route path='/groups/:groups_id/inicialmembers' element={ <MemberInicialCreate /> } />
-          <Route path='/groups/:groups_id/members' element={ <MemberDisplay /> } />
-          <Route path='/groups/:groups_id/transactions' element={ <TransactionDisplay /> } />
+          <Route path='/groups/:group_id' element={ <Dashboard /> } />
+          <Route path='/groups/:group_id/debts' element={ <Debts /> } />
+          <Route path='/groups/:group_id/settle/new' element={ <Settle /> } />
+          <Route path='/groups/:group_id/transactions' element={ <TransactionDisplay /> } />
+          <Route path='/groups/:group_id/transactions/new' element={ <TransactionCreate /> } />
+          <Route path='/groups/:group_id/transactions/:id' element={ <TransactionEdit /> } />
+          <Route path='/groups/:group_id/members' element={ <MemberDisplay /> } />
+          <Route path='/groups/:group_id/members/new' element={ <MemberCreate /> } />
+          <Route path='/groups/:group_id/members/:id' element={ <MemberEdit /> } />
+          <Route path='/groups/:group_id/inicialmembers' element={ <MemberInicialCreate /> } />
         </Routes>
       </BrowserRouter>
     </div>
@@ -25,3 +37,18 @@ function App() {
 }
 
 export default App;
+
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />}>
+//         <Route index element={<Home />} />
+//         <Route path="teams" element={<Teams />}>
+//           <Route path=":teamId" element={<Team />} />
+//           <Route path="new" element={<NewTeamForm />} />
+//           <Route index element={<LeagueStandings />} />
+//         </Route>
+//       </Route>
+//     </Routes>
+//   </BrowserRouter>
+// );
