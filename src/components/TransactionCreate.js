c477644b3dfee2a9df4e634926061185f9a4f4ae
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-31 12:01:05 +1000

    add modal to display transaction create

--- src/components/TransactionCreate.js
+++ src/components/TransactionCreate.js
@@ -37,62 +37,106 @@ const TransactionCreate = (props) => {
             body: JSON.stringify(transaction),
         })
         .then((resp) => resp.json())
-        .then((transaction) => navigate(-1))
+        .then((transaction) => navigate(`/groups/${ params.group_id }/transactions`))
     };
-
-    useEffect(() => {
-        fetch(`http://localhost:3000/groups/${ params.group_id }/members`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then((resp) => resp.json())
-        .then((members) => setMembersGroup(members))
-    }, []);
 
     const _handleChange = (event) => {
         setTransaction((prevTransaction) => ({
             ...prevTransaction, [event.target.name]: event.target.value
         })
     )};
+
+    const _handleClick = () => {
+        navigate(`/groups/${ params.group_id }/transactions`);
+    }
     
     return(
-        <div className="container">
-            <p className="title-transaction">New expense</p>
-            <div className="form-container">
-                <form onSubmit={_handleSubmit}>
-                    {/* <select name="kind" value={transaction.kind} required */}
-                        {/* onChange={_handleChange}> */}
-                        {/* <option value="">Choose type of transaction</option> */}
-                        {/* <option value="expense">Expense</option> */}
-                        {/* <option value="transfer">Transfer</option> */}
-                        {/* <option value="income">Income</option> */}
-                    {/* </select> */}
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">New expense</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={_handleClick}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                <div className="modal-body">
+                    <form onSubmit={_handleSubmit}>
+                        {/* <select name="kind" value={transaction.kind} required onChange={_handleChange}> */}
+                            {/* <option value="">Choose type of transaction</option> */}
+                            {/* <option value="expense">Expense</option> */}
+                            {/* <option value="transfer">Transfer</option> */}
+                            {/* <option value="income">Income</option> */}
+                        {/* </select> */}
 
-                    <select name="member_id" value={transaction.member_id} required className="form-select form-select-sm" 
-                        onChange={_handleChange}>
-                        <option value=''>Who paid</option>
-                        {membersGroup.map((member) => (
-                            <option key={member.id} value={member.id}>{member.name}</option>
-                        ))}
-                    </select>
+                        <select name="member_id" value={transaction.member_id} required className="form-control" 
+                            onChange={_handleChange}>
+                                <option value=''>Who paid</option>
+                                {membersGroup.map((member) => (
+                                    <option key={member.id} value={member.id}>{member.name}</option>
+                                ))}
+                        </select>
 
-                    <input name="description" value={transaction.description} className="form-control"
-                        onChange={_handleChange} placeholder="Description" />
-                                
-                    <input name="amount" value={transaction.amount} required className="form-control"
-                        onChange={_handleChange} placeholder="$100.00" />
-                    
-                    <input name="date" value={transaction.date} type="date" required className="form-control"
-                        onChange={_handleChange} />
-                    {/* <input name="image" value={transaction.image} placeholder="Image"
-                        onChange={_handleChange} /> */}
+                        <input name="description" value={transaction.description} className="form-control"
+                            onChange={_handleChange} placeholder="Description" />
+                                    
+                        <NumberFormat name="amount" value={transaction.amount} required className="form-control" 
+                        onChange={_handleChange} thousandSeparator={true} placeholder="$100.00" />
+                        
+                        <input name="date" value={transaction.date} type="date" required className="form-control"
+                            onChange={_handleChange} />
 
-                    <button className="btn btn-primary btn-lg btn-block">Save</button>
-                </form>
+                        {/* <input name="image" value={transaction.image} placeholder="Image"
+                            onChange={_handleChange} /> */}
+
+                        <button className="btn btn-primary btn-lg btn-block">Save</button>
+                    </form>
+                </div>
+                {/* <div className="modal-footer"> */}
+                    {/* <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> */}
+                    {/* <button className="btn btn-primary btn-lg btn-block">Save</button> */}
+                {/* </div> */}
+                </div>
             </div>
         </div>
+        // <div className="container">
+        //     <p className="title-transaction">New expense</p>
+        //     <div className="form-container">
+        //         <form onSubmit={_handleSubmit}>
+        //             {/* <select name="kind" value={transaction.kind} required */}
+        //                 {/* onChange={_handleChange}> */}
+        //                 {/* <option value="">Choose type of transaction</option> */}
+        //                 {/* <option value="expense">Expense</option> */}
+        //                 {/* <option value="transfer">Transfer</option> */}
+        //                 {/* <option value="income">Income</option> */}
+        //             {/* </select> */}
+
+        //             <select name="member_id" value={transaction.member_id} required className="form-select form-select-sm" 
+        //                 onChange={_handleChange}>
+        //                 <option value=''>Who paid</option>
+        //                 {membersGroup.map((member) => (
+        //                     <option key={member.id} value={member.id}>{member.name}</option>
+        //                 ))}
+        //             </select>
+
+        //             <input name="description" value={transaction.description} className="form-control"
+        //                 onChange={_handleChange} placeholder="Description" />
+                                
+        //             <NumberFormat name="amount" value={transaction.amount} required className="form-control" 
+        //              onChange={_handleChange} thousandSeparator={true} prefix={'$'} placeholder="$100.00" />
+                    
+        //             <input name="date" value={transaction.date} type="date" required className="form-control"
+        //                 onChange={_handleChange} />
+
+        //             {/* <input name="image" value={transaction.image} placeholder="Image"
+        //                 onChange={_handleChange} /> */}
+
+        //             <button className="btn btn-primary btn-lg btn-block">Save</button>
+        //         </form>
+        //         <button className="btn btn-primary btn-lg btn-block"
+        //                 onClick={_handleClick}>Cancel</button>
+        //     </div>
+        // </div>
     );
 };
 
--- src/components/TransactionDisplay.js
+++ src/components/TransactionDisplay.js
@@ -31,22 +31,33 @@ const TransactionDisplay = (props) => {
     
     return(
         <div className="container">
-            <p className="title-transaction">Transactions</p>
-            <div className="transaction">
+            <div className="header">
+                <p className="title3">Transactions</p>
+                <Link to={`/groups/${ params.group_id }/transactions/new`}>
+                    <button className="btn btn-light" type="button" data-toggle="modal" data-target="#exampleModal">
+                        + transaction
+                    </button>
+                </Link>
+            </div>
+
+            <div className="list">
                 { transactionsGroup.sort((a, b) => b.id - a.id).map((transaction) => (
                     <Link to={`/groups/${ params.group_id }/transactions/${ transaction.id }`}
                         key={transaction.id} state={ transaction }>
                         <button className="btn btn-primary btn-lg btn-block btn-display">
                             <div className="transaction-info">
                                 <div className="transaction-info one">
-                                    <span className="kind">{(transaction.kind).charAt(0).toUpperCase() + (transaction.kind).slice(1)}</span>
+                                    <span className="kind">{(transaction.kind).charAt(0).toUpperCase() + (transaction.kind).slice(1)}: {transaction.description}</span>
                                     <span className="date">{new Date(transaction.date).toLocaleDateString('en-us', { day:"numeric", month:"short", year:"numeric" })}</span>
                                 </div>
 
                                 <div className="transaction-info two">
-                                    <span>description: {transaction.description}</span>
-                                    <span>$ {new Intl.NumberFormat().format(transaction.amount)}</span>
-                                    <span>member: {transaction.member_id}</span>
+                                    <span>{new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(transaction.amount)}</span>
+                                    {membersGroup.map((member) => {
+                                        if (transaction.member_id === member.id) {
+                                            return <span key={transaction.id}>{member.name}</span>
+                                        }
+                                    })}
                                 </div>
                             </div>
                         </button>
@@ -54,13 +65,7 @@ const TransactionDisplay = (props) => {
                 ))}
             </div>
 
-            <Link to={`/groups/${ params.group_id }/transactions/new`}>
-                <button className="btn btn-primary btn-lg btn-block"
-                    data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo"
-                >
-                    + transaction
-                </button>
-            </Link>
+
         </div>
     );
 };
